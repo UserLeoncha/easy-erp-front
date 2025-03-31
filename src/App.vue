f18ca9d2d7044af51d8001562e24f0d092958d54
李昂 <110752532+UserLeoncha@users.noreply.github.com>
2025-03-31 20:26:58 +0800

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <el-config-provider :size="size" :z-index="zIndex">
+    <app />
+  </el-config-provider>
+</template>
+
+<script>
+import { defineComponent } from 'vue'
+import { ElConfigProvider } from 'element-plus'
+
+export default defineComponent({
+  components: {
+    ElConfigProvider,
+  },
+  setup() {
+    return {
+      zIndex: 3000,
+      size: 'small',
+    }
+  },
+})
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
